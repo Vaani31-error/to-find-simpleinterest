550dd5f81d5c992c8dda14d2e126db5a8706741e
Vani Sharma <sharmavaani219@gmail.com>
2024-08-01 12:55:13 +0530

    simple interest

---
+++ simpleintrest.py
@@ -0,0 +1,9 @@
+#Write a program to find the simple interest
+#principal amount=1000
+#rate of interest=5%
+#time=2 year
+value_principal=1000
+rate_interest=5
+time=2
+simple_interest=float((value_principal*rate_interest*time/100))
+print(simple_interest)
